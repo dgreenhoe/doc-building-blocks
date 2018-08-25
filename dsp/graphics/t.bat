c03d42d938f04ac48465c88cbe85f4c4270063e6
Daniel J. Greenhoe <dgreenhoe@gmail.com>
2018-08-25 11:35:46 +0000

    update archive

--- dsp/graphics/t.bat
+++
@@ -1,4 +0,0 @@
-copy c:\dan\r\psd\graphics\shell_lat2_l2_setx.tex      . /v
-copy c:\dan\r\psd\graphics\shell_lat4_l2e2_setxy.tex   . /v
-copy c:\dan\r\psd\graphics\shell_lat4_m2_setxy.tex     . /v
-copy c:\dan\r\psd\graphics\shell_lat8_2e3_setxyz.tex   . /v
