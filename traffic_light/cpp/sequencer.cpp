77bdfd012b91649370e926a5f51f01edb1560e55
Daniel J. Greenhoe <dgreenhoe@gmail.com>
2019-09-12 08:37:10 +0000

    traffic light ASCII graphic

--- traffic_light/cpp/sequencer.cpp
+++ traffic_light/cpp/sequencer.cpp
@@ -2,6 +2,39 @@
 //! \author  Daniel J. Greenhoe
 //! \class   Traffic-Light-Control
 //! \brief   Sequence 4 face 5 light/face traffic light
+//! \details The traffic light is assumed to have 4 faces:
+//!          master, back, left, right.
+//! \code{.markdown}
+//!             _________________
+//!            /.               /|
+//!           / .              / |
+//!          /  .             /  |
+//!         /   .            /   |
+//!        /________________/   D|
+//!        |    .  ______   |  E |
+//!        |    . (      )  | R  |
+//!        | R  . (  RED )  |    |
+//!        |    . (______)  |    |
+//!        |    .           |   .|
+//!        |    .  ______   |  L |
+//!        |    . (      )  | E  |
+//!        | Y  . (YELLOW)  |Y   |
+//!        |    . (______)  |    |
+//!        |    .           |   .|
+//!        |    .  ______   |  E |
+//!        |    . (      )  | R  |
+//!        | G  . (GREEN )  |G   |
+//!        |    . (______)  |    |
+//!        |    .           |    |
+//!        |    .  ______   |back|
+//!        |    .-(ARROW )--|----
+//!       l|eft.  ( <=== )  |   /
+//!        |  .   (______)  |  /
+//!        | .              | /right
+//!        |._______________|/
+//!              master
+//!
+//! \endcode
 //=============================================================================
 #include "sequencer.h"
 //-----------------------------------------------------------------------------
@@ -22,3 +55,11 @@ faceState step_masterLight(
   }
   return next;
   }
+
+//-----------------------------------------------------------------------------
+//! \brief Step master light state variable
+//-----------------------------------------------------------------------------
+faceState step_masterLight(
+  faceState state  //! \param[in] state: current state of master light
+  )
+  {
