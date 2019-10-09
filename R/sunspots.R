2c911fb6a514a664177bbd7ef0f083e851409164
Daniel J. Greenhoe <dgreenhoe@gmail.com>
2019-10-09 11:31:26 +0000

    motivation for moment estimation

--- R/sunspots.R
+++ R/sunspots.R
@@ -21,17 +21,32 @@
 # load data
 #---------------------------------------
  x = sunspot.month;
+#---------------------------------------
+# calculate average
+#---------------------------------------
  N = length(x);
  avg = sum(x) / N;
  print(avg);
- mu = mean(x);
+#---------------------------------------
+# calculate average recursively
+#---------------------------------------
+ muhat = seq(from=0, to=0, length=N);
+ muhat[1] = x[1];
+ w = seq(from=2, to=N, length=N-1);
+N=100;
+ muhat[2:N] = muhat[1:N-1] + (x[2:N] - muhat[1:N-1]) / w[1:N-1];
+print(muhat[1:N]);
+#---------------------------------------
+# plot data
+#---------------------------------------
  plot(x, col="blue", type="l");
  lines(c(start(x)[1],end(x)[1]), c(avg,avg), col="red", type="l");
+ lines(muhat, col="green", type="l");
 #---------------------------------------
 # load data
 #---------------------------------------
  class(x); # make sure x is of type time-series ("ts")
- x_psd = bspec::welchPSD(x, N/4);
+# x_psd = bspec::welchPSD(x, N/4);
 #plot(x_psd$power, type="l", xlim=c(1850,2019), ylim=c(0,100), col="red")
 #---------------------------------------
 # process data
