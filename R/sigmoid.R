9841ee5cf36744f8d5f8a74d04083b048da1fa33
Daniel J. Greenhoe <dgreenhoe@gmail.com>
2021-02-07 06:02:33 -0500

    stack exchange plant-growth, sigmoid

--- R/sigmoid.R
+++ R/sigmoid.R
@@ -48,17 +48,6 @@ sigmoid_tanh = function(x,slope)
 }
 
 #---------------------------------------
-# Logistic sigmoid function
-#---------------------------------------
-sigmoid_intlogp = function(x,slope,p)
-{
-   a = slope
-  #b = 2 * slope / p
-   b = slope;
-   result = 1 - (1 / (1 + a*exp(-b*x)))^p 
-}
-
-#---------------------------------------
 # Logistic sigmoid to power p
 #---------------------------------------
 sigmoid_logp = function(x,slope,p)
@@ -66,16 +55,15 @@ sigmoid_logp = function(x,slope,p)
    a = 2^(1/p)-1
    b = slope * (1 + a)^(2 + p -1) / (a * p )
    result = (1 / (1 + a*exp(-b*x)))^p 
-  #result = 1-(1 / (1 + a*exp(b*x)))^p 
 }
 
 #---------------------------------------
-# Logistic sigmoid function
+# tanh^p(bx)
 #---------------------------------------
-sigmoid_tanhp = function(x,b,p)
-{
-   result = 0.5 * (((exp(b*x) - exp(-b*x)) / (exp(b*x) + exp(-b*x)))^p + 1)
-}
+#tanhp = function(x,b,p)
+#{
+#   result = 0.5 * (((exp(b*x) - exp(-b*x)) / (exp(b*x) + exp(-b*x)))^p + 1)
+#}
 
 #---------------------------------------
 # Logistic sigmoid function
@@ -83,7 +71,7 @@ sigmoid_tanhp = function(x,b,p)
 # https://archive.org/details/integralstable00peirrich/page/59/
 # https://archive.org/details/integralstable00peirrich/page/81/
 #---------------------------------------
-sigmoid_inttanhp = function(x,b,p)
+tanhp = function(x,b,p)
 {
   #result = 1 - ((exp(b*x) - exp(-b*x)) / (exp(b*x) + exp(-b*x)))^p
    result = 1 - (tanh(b*x))^p
@@ -91,32 +79,12 @@ sigmoid_inttanhp = function(x,b,p)
 
 #---------------------------------------
 # Logistic sigmoid function
-# https://en.wikipedia.org/wiki/List_of_integrals_of_hyperbolic_functions
-#---------------------------------------
-sigmoid_inttanhpx = function(x,b,p)
-{
-   intb = 0
-   for( n in seq(from=p, to=2, by=-2))
-   {
-     intb = intb + 1/(b*(n-1))*(-1)^(n-1)
-   }
-   total = 0
-   for( n in seq(from=p, to=2, by=-2))
-   {
-     total = total + 1/(b*(n-1))*(tanh(b*x))^(n-1)
-   }
-   total = total-intb
-   result = total
-}
-
-#---------------------------------------
-# Logistic sigmoid function
 # https://ia800806.us.archive.org/7/items/GradshteinI.S.RyzhikI.M.TablesOfIntegralsSeriesAndProducts/Gradshtein_I.S.%2C_Ryzhik_I.M.-Tables_of_integrals%2C_series_and_products.pdf
 # 2.424 (3) page 119
 #---------------------------------------
-inttanhpxx = function(x,b,n)
+inttanhp = function(x,b,p)
 {
-   p = 2*n
+   n = p/2
    inta = 0
    for( k in c(1:n) )
    {
@@ -135,22 +103,12 @@ inttanhpxx = function(x,b,n)
 #---------------------------------------
 # Display
 #---------------------------------------
- colors = c( "blue"   , "red", "orange", "black", "purple","green");
- traces = c( "sigmoid", "sigmoid^(1/2)", "sigmoid^(1/3)", "sigmoid^(1/4)", "sigmoid^(1/5)" );
-# plot ( x, sigmoid_inttanhp(x,2,13) , col=colors[1], lwd=3, type='l', xlab="x", ylab="y" )
- plot ( x, sigmoid_inttanhp(x,1/(0.55-0.45),3090) , col=colors[1], lwd=3, type='l', xlab="x", ylab="y", ylim=c(-0.5,1.5) )
-#lines ( x, sigmoid_inttanhpx(x,1/(0.55-0.45),6000) , col=colors[2], lwd=3, type='l' )
- lines ( x, inttanhpxx(x,1/(0.55-0.45),3090) , col=colors[3], lwd=3, type='l' )
-# plot ( x, sigmoid_inttanhpx(x,10,10) , col=colors[2], lwd=3, type='l')
-#lines ( x, sigmoid_tanhp(x,1,3) , col=colors[2], lwd=3, type='l', xlab="x", ylab="y" )
- #lines( x, linear(x,1,0)       , col=colors[3], lwd=2, type='l' )
-# plot ( x, sigmoid_tanh(x,1) , col=colors[1], lwd=3, type='l', xlab="x", ylab="y", ylim=c(-1,1), xaxp  = c(-1, 1, 20), yaxp=c(0,1,10))
-#plot ( x, sigmoid_logistic(x,1) , col=colors[1], lwd=3, type='l', xlab="x", ylab="y", ylim=c(0,1), xaxp  = c(-1, 1, 20), yaxp=c(0,1,10))
-#lines( x, sigmoid_logp(x,1,1/2) , col=colors[2], lwd=2, type='l' )
-#lines( x, sigmoid_logp(x,1,1/3) , col=colors[3], lwd=2, type='l' )
-#lines( x, sigmoid_logp(x,1,1/4) , col=colors[4], lwd=2, type='l' )
-#lines( x, sigmoid_logp(x,1,1/10), col=colors[5], lwd=2, type='l' )
- legend("topleft", legend=traces, col=colors, lwd=3, lty=1:1)
- grid(col="green", lwd=2, lty="dashed",nx=NULL)
-# grid(nx=11, ny=11)
+ colors = c( "blue", "red", "orange", "black", "purple","green");
+#plot ( x, tanhp(x,2,1) , col=colors[1], lwd=3, type='l', xlab="x", ylab="y" )
+#plot ( x, tanhp(x,1/(0.55-0.45),6180) , col=colors[1], lwd=3, type='l', xlab="x", ylab="y", ylim=c(-0.1,1.1), xlim=c(-1.5,1.5))
+ plot ( x, inttanhp(x,1/(0.55-0.45),6180) , col=colors[2], lwd=3, type='l', xlab="x", ylab="y" )
+#lines( x, linear(x,1,1/2)       , col=colors[6], lwd=2, type='l' )
+#lines( x, linear(x,0,1/2)       , col=colors[6], lwd=2, type='l' )
+ legend("topleft", legend="f(x); b=10; p=6180", col="red", lwd=3, lty=1:1)
+ grid()
 
