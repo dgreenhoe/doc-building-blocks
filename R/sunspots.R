a7b4564af6263953704af23224b90e59d962f8a5
Daniel J. Greenhoe <dgreenhoe@gmail.com>
2021-05-02 07:42:25 -0400

    sunspots_PCA_PSD

--- R/sunspots.R
+++ R/sunspots.R
@@ -13,6 +13,7 @@
 #install.packages("ramify");
 #install.packages("freqdom");
 #install.packages("matlib");
+#install.packages("R.utils");
 #---------------------------------------
 # load add-on packages
 #---------------------------------------
@@ -24,12 +25,14 @@
  require(freqdom);
  require(bspec);    # https://www.rdocumentation.org/packages/bspec/
  require(matlib);
+ require(R.utils);
  data(sunspots, package="datasets");
-#=======================================
-# function: data
-#=======================================
+
+#------------------------------------------------------------------------------
+# \brief Plot data
+#------------------------------------------------------------------------------
 mydata = function(x)
-  {
+{
   N = length(x);
   avg = sum(x) / N;
   print(avg);
@@ -37,12 +40,13 @@ mydata = function(x)
   lines(c(start(x)[1],end(x)[1]), c(avg,avg), col="red", type="l");
   x_smooth = smooth(x, kind="3R");
   lines(x_smooth, col="green", type="l");
-  }
-#=======================================
-# function: PSD
-#=======================================
-mypsd = function(x, numSegments=4, dataDump=FALSE, dataPlot=TRUE, dataFile="mypsd.dat") 
-  {
+}
+
+#------------------------------------------------------------------------------
+# \brief Estimate sunspot period using Welch Estimate of PSD
+#------------------------------------------------------------------------------
+sunspots_PSD = function(x, numSegments=4, dataDump=FALSE, dataPlot=TRUE, dataFile="mypsd.dat")
+{
   xts       = as.ts(as.vector(x)); # year indices seems to confuse welchPSD
   N         = length(xts);         # length of time series
   Fs        = 12;                  # sample rate = 12 samples per year
@@ -53,7 +57,10 @@ mypsd = function(x, numSegments=4, dataDump=FALSE, dataPlot=TRUE, dataFile="myps
   binMax = ramify::argmax(as.matrix(xpsd$power), rows = FALSE);
   freqMax = xpsd$frequency[binMax] * Fs; # dominate non-DC frequency
   periodT = 1 / freqMax;           # estimated period
-  if(dataPlot){ 
+  printf("sunspots_PSD(x):\n");
+  printf("f=%8.6f samples/year period=%9.6f years\n", freqMax, periodT);
+
+  if(dataPlot){
     plot(xpsd$frequency*Fs, sqrt(xpsd$power/N), xlim=c(0,1), type="h", col="blue", xlab="samples/year");
     }
   if(dataDump){
@@ -65,22 +72,25 @@ mypsd = function(x, numSegments=4, dataDump=FALSE, dataPlot=TRUE, dataFile="myps
     print(sprintf("%% estimated period   = %12.6f", periodT                                        ));
     print(sprintf("%%============================================================================="));
     print(sprintf("["                                                                              ));
-    for(i in 1:length(xpsd$power)) 
+    for(i in 1:length(xpsd$power))
       print(sprintf("(%10.6f, %10.6f)", xpsd$frequency[i], xpsd$power[i]/psdMax               ));
     print(sprintf("]"                                                                              ));
     sink();
     }
   periodT;                         # return estimated period
-  }
-#=======================================
+}
+
+#------------------------------------------------------------------------------
 # function: pdf
-#=======================================
-mypdf = function(x){
+#------------------------------------------------------------------------------
+mypdf = function(x)
+{
   plot(density(x, bw="SJ"))
-  }
-#=======================================
+}
+
+#------------------------------------------------------------------------------
 # function: pdf recursive
-#=======================================
+#------------------------------------------------------------------------------
 pdfr = function(x)
 {
   N = length(x);
@@ -90,32 +100,18 @@ pdfr = function(x)
   muhat[2:N] = muhat[1:N-1] + (x[2:N] - muhat[1:N-1]) / w[1:N-1];
   print(muhat[1:10]);
 }
-#---------------------------------------
-# load data
-#---------------------------------------
- x = sunspot.month;
-#---------------------------------------
-# plot data
-#---------------------------------------
- mydata(x);
- #estT = mypsd(x, numSegments=8, dataDump=FALSE);
-# print(sprintf("estimated period = %.16f years", estT));
 
-
-#mypdf(x);
-#pdfr(x);
-#---------------------------------------
+#------------------------------------------------------------------------------
+# \brief Estimate sunspot period using PCA and Welch Estimate of PSD of PCA
 # PCA
 # https://cran.r-project.org/web/packages/matlib/vignettes/inv-ex1.html
 # https://cran.r-project.org/web/packages/matlib/vignettes/inv-ex1.html
 # https://stat.ethz.ch/R-manual/R-patched/library/base/html/eigen.html
 # https://en.wikipedia.org/wiki/Eigendecomposition_of_a_matrix
-#---------------------------------------
-
-numSegments=4
-dataDump=FALSE
-dataPlot=TRUE
-dataFile="mypsd.dat" 
+#------------------------------------------------------------------------------
+sunspots_PCA_PSD = function(x, numSegments=4)
+{
+  numSegments=4
   xts       = as.ts(as.vector(x)); # year indices seems to confuse welchPSD
   N         = length(xts);         # length of time series
   Fs        = 12;                  # sample rate = 12 samples per year
@@ -128,8 +124,34 @@ dataFile="mypsd.dat"
   V = Q$vectors
   L = Q$values
   D = diag(L)
-  plot( L[1] * Q$vectors[,1], type='o', col="blue")
-  lines(L[2] * Q$vectors[,2], type='o', col="red")
-  lines(L[3] * Q$vectors[,3], type='o', col="orange")
-  lines(L[4] * Q$vectors[,4], type='o', col="brown")
-  lines(L[5] * Q$vectors[,5], type='o', col="black")
+
+  colors = c("blue", "red", "orange", "green", "purple", "brown", "black");
+  plot( L[1] * Q$vectors[,1], type='o', col=colors[1])
+  for( n in 2:5 )
+  {
+    lines(L[n] * Q$vectors[,n], type='o', col=colors[n])
+  }
+
+  printf("sunspots_PCA_PSD(x):\n");
+  for( n in 1:10 )
+  {
+    xpsd = bspec::welchPSD(x=as.ts(L[n] * Q$vectors[,n]), seglength = segLength);
+    psdMax = max(xpsd$power);
+    binMax = ramify::argmax(as.matrix(xpsd$power), rows = FALSE);
+    freqMax = xpsd$frequency[binMax] * Fs; # dominate non-DC frequency
+    periodT = 1 / freqMax;           # estimated period
+    printf("Vector %2d (lambda=%10.6f) f=%8.6f samples/year period=%9.6f years\n", n, L[n], freqMax, periodT);
+  }
+}
+
+#---------------------------------------
+# load data
+#---------------------------------------
+ x = sunspot.month;
+#---------------------------------------
+# plot data
+#---------------------------------------
+ mydata(x);
+ sunspots_PSD(x)
+ sunspots_PCA_PSD(x)
+
